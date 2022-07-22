04fd1cf290f8ad1f2fd4646be8f527ca8cce5e95
Kasioautomat <109282307+Kasioautomat@users.noreply.github.com>
2022-07-22 11:30:41 +0200

    Main

---
+++ WebDriverUnivercity.spec.cy.js
@@ -0,0 +1,14 @@
+/// <reference types="cypress" />
+
+describe('Web Driver Univercity', () => {
+
+it ('1st test for Dropdown Menu(s), Checkboxe(s) & Radio Button(s)', () => {
+
+cy.visit('https://webdriveruniversity.com/Dropdown-Checkboxes-RadioButtons/index.html')
+
+})
+
+  })
+
+
+});
